a9a337b48b04fab97d8ece109f56a38837ea89f7
Pat Putnam <prp2986@rit.edu>
2016-11-30 18:18:06 -0500

    Add “auth” functionality for a protected endpoint

--- routes/items.js
+++ routes/items.js
@@ -7,9 +7,29 @@ var items = [
   { name: 'item 3' }
 ];
 
+var auth = function(req, res, next) {
+  var name = req.query.name;
+  if(name === 'Dan') {
+    next();
+  } else {
+    var err = new Error('Unauthorized');
+    err.status = 403;
+    next(err);
+  }
+};
+
+router.use('/', function(req, res, next) {
+  console.log('hit!');
+  next();
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('items', { title: 'Item List', items: items });
+});
+
+router.get('/protected', auth, function(req, res, next) {
+  res.render('items', { title: 'Protected Item List', items: items });
 });
 
 module.exports = router;

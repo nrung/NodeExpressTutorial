e5331e0a8e2255e7373d1b2ebb720ee57b1e63d6
Pat Putnam <prp2986@rit.edu>
2016-11-30 21:26:26 -0500

    Remove Users

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 var items = require('./routes/items');
 
 var app = express();
@@ -24,7 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 app.use('/items', items);
 
 // catch 404 and forward to error handler
